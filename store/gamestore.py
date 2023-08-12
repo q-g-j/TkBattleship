f9e6cbdd6252660b4af3bf32e9f91bf21b0a6de4
Jann Emken <qgj.devel@gmail.com>
2023-08-12 14:13:05 +0200

    add type hint to GameStore

--- store/gamestore.py
+++ store/gamestore.py
@@ -1,3 +1,9 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models.game import Game
+
+
 class GameStore:
     def __init__(self):
-        self.game = None
+        self.game: Game | None = None
