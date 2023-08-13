15ef6f0020c0f69a9261d50622896fc0e29319d6
QGJ <qgj.devel@gmail.com>
2023-08-14 00:00:09 +0200

    remove print

--- utils/oshelper.py
+++ utils/oshelper.py
@@ -7,7 +7,6 @@ class OsHelper:
     @staticmethod
     def get_os() -> OS:
         operating_system = platform.system()
-        print(operating_system)
         if operating_system in [eval(f"OS.{member}") for member in dir(OS)]:
             return operating_system
         else:
